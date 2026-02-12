c9e3b7bde81fd7a8b6f3062ea7f9c733a3654c90
Zacck Osiemo <coderv63@gmail.com>
2026-02-12 16:51:07 +0200

    Add simulation for this blinky app
    Signed-off-by: Zacck Osiemo <coderv63@gmail.com>

---
+++ registers/blinky/blinky.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Vblinky.h"
+#include "verilated.h"
+
+//clock toggler for our simulation
+void tick(Vblinky *tb) {
+	tb->eval(); 
+	tb->i_clk = 1; 
+	tb->eval(); 
+	tb->i_clk = 0; 
+	tb->eval();
+}
+
+
+
+int main(int argc, char **argv) {
+	Verilated::commandArgs(argc, argv); 
+	Vblinky *tb = new Vblinky;
+
+	int last_led; 
+	last_led  = tb->o_led; 
+	for(int k=0; k <(1<<10); k++) {
+		tick(tb);
+		if (last_led != tb->o_led) {
+			printf("k = %7d ", k);
+			printf("led = %d\n", tb->o_led);
+		} last_led = tb->o_led;
+
+	}
+}
+
+
+
+
