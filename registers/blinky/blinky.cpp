fb33832ce3679a73cfd851e5c27f216b1dcbc430
Zacck Osiemo <coderv63@gmail.com>
2026-02-12 19:21:28 +0200

    Implement vcd wavedumping
    Signed-off-by: Zacck Osiemo <coderv63@gmail.com>

--- registers/blinky/blinky.cpp
+++ registers/blinky/blinky.cpp
@@ -2,15 +2,23 @@
 #include <stdlib.h>
 #include "Vblinky.h"
 #include "verilated.h"
-
+#include "verilated_vcd_c.h"
 
 //clock toggler for our simulation
-void tick(Vblinky *tb) {
+void tick(int tickcount, Vblinky *tb, VerilatedVcdC *tfp) {
 	tb->eval(); 
+	if(tfp)//dump 2ns before the tick
+	       tfp->dump(tickcount * 10 - 2);
 	tb->i_clk = 1; 
 	tb->eval(); 
+	if(tfp) //tick every 10ns
+		tfp->dump(tickcount * 10);
 	tb->i_clk = 0; 
 	tb->eval();
+	if(tfp) { //trailing edge dump 
+		  tfp->dump(tickcount * 10 + 5); 
+		  tfp->flush(); 
+	}
 }
 
 
@@ -19,9 +27,16 @@ int main(int argc, char **argv) {
 	Verilated::commandArgs(argc, argv); 
 	Vblinky *tb = new Vblinky;
 
+	// trace generation
+	unsigned tickcount = 0;
+	Verilated::traceEverOn(true);
+	VerilatedVcdC *tfp = new VerilatedVcdC;
+	tb->trace(tfp, 99);
+	tfp->open("blinkytrace.vcd");
+
 	int last_led = tb->o_led; 
 	for(int k=0; k <(1<<20); k++) {
-		tick(tb);
+		tick(++tickcount, tb, tfp);
 		if (last_led != tb->o_led) {
 			printf("k = %7d ", k);
 			printf("led = %d\n", tb->o_led);
