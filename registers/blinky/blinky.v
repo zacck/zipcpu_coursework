6ab2630b5c9a6de236898fec1555a9cd4941d0a2
Zacck Osiemo <coderv63@gmail.com>
2026-02-13 07:23:07 +0200

    Formating of HDL
    Signed-off-by: Zacck Osiemo <coderv63@gmail.com>

--- registers/blinky/blinky.v
+++ registers/blinky/blinky.v
@@ -1,22 +1,20 @@
 `default_nettype none
-module blinky
- (
-	i_clk, 
-	o_led
+module blinky (
+    i_clk,
+    o_led
 );
 
- parameter WIDTH = 27;
- input wire i_clk;
- output wire o_led;
+  parameter WIDTH = 27;
+  input wire i_clk;
+  output wire o_led;
 
 
 
- reg [WIDTH-1:0] counter; 
+  reg [WIDTH-1:0] counter;
 
- initial counter = 0; 
+  initial counter = 0;
 
- always @(posedge i_clk)
-	 counter <= counter + 1'b1;
+  always @(posedge i_clk) counter <= counter + 1'b1;
 
- assign o_led = counter[WIDTH-1];
+  assign o_led = counter[WIDTH-1];
 endmodule
