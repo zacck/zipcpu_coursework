475841200ba883d8aa1be35cda7ca83b1805411f
Zacck Osiemo <coderv63@gmail.com>
2026-02-14 07:30:14 +0200

    Add comments to understand what is happening
    Signed-off-by: Zacck Osiemo <coderv63@gmail.com>

--- fsm/ledwalker/ledwalker.v
+++ fsm/ledwalker/ledwalker.v
@@ -8,11 +8,15 @@ module ledwalker (
   reg [31:0] counter;
   reg stb;
 
+  // set defaults
   initial {stb, counter} = 0;
   initial o_led = 9'b000000001;
 
+  /* Implement 1hz strobe signal
+   * Only every true on counter overrun
+   * Counter overruns once a second
+   */
   parameter integer CLK_RATE_HZ = 12_000_000;
-
   always @(posedge i_clk)
     if (counter == 0) counter <= CLK_RATE_HZ - 1;
     else counter <= counter - 1'b1;
@@ -22,6 +26,9 @@ module ledwalker (
     if (counter == 0) stb <= 1'b1;
   end
 
+  /* Implement a 1 bit Shift register
+   * Shift MSB towards LSB every strobe cycle
+   */
   always @(posedge i_clk) if (stb) o_led <= {o_led[7:0], o_led[8]};
 
 endmodule
